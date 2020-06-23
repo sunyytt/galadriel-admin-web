a3408e1481ed0a8d578ed4b77ff9beb9ca204a66
sunyy <407012881@qq.com>
2020-06-23 14:41:44 +0800

    右边工具栏显示

--- src/views/flowable/bpmn/index.vue
+++ src/views/flowable/bpmn/index.vue
@@ -1,38 +1,36 @@
 <template>
-<!--  <div class="components-container">-->
-<!--    <aside><strong>集成bpmn.js</strong></br>-->
-<!--      1.安装以下包进开发环境-->
-<!--      npm install &#45;&#45;save bpmn-js-->
-<!--      npm install &#45;&#45; save bpmn-js-properties-panel-->
-<!--      npm install &#45;&#45;save camunda-bpmn-moddle </br>-->
-<!--      2.定制开发以下功能：</br>-->
-<!--      （1）打开：打开本地的*.bpmn20.xml模型文件,并在bpmn.js插件的画布上显示出来。-->
-<!--      （2）创建：创建一个新的流程，在画布上供用户拖拉完成。-->
-<!--      （3）导出流程模板：导成一个xml或者zip的形式。-->
-<!--      （4）撤销：支持向前或向后撤销，即撤销在画布上刚才操作。-->
-<!--      (5) 放大/缩小：支持画布的放大与缩小，以及重置恢常正常大小。-->
-<!--      (6) 保存流程的模型到自己的定义数据库表，同时同步更新到act_de_model表中。-->
-<!--      (7) 节点属性面板的定制功能开发。-->
-<!--    </aside>-->
-<!--  </div>-->
   <div class="containers">
+    <!--    <aside><strong>集成bpmn.js</strong></br>-->
+    <!--      1.安装以下包进开发环境-->
+    <!--      npm install &#45;&#45;save bpmn-js-->
+    <!--      npm install &#45;&#45; save bpmn-js-properties-panel-->
+    <!--      npm install &#45;&#45;save camunda-bpmn-moddle </br>-->
+    <!--      2.定制开发以下功能：</br>-->
+    <!--      （1）打开：打开本地的*.bpmn20.xml模型文件,并在bpmn.js插件的画布上显示出来。-->
+    <!--      （2）创建：创建一个新的流程，在画布上供用户拖拉完成。-->
+    <!--      （3）导出流程模板：导成一个xml或者zip的形式。-->
+    <!--      （4）撤销：支持向前或向后撤销，即撤销在画布上刚才操作。-->
+    <!--      (5) 放大/缩小：支持画布的放大与缩小，以及重置恢常正常大小。-->
+    <!--      (6) 保存流程的模型到自己的定义数据库表，同时同步更新到act_de_model表中。-->
+    <!--      (7) 节点属性面板的定制功能开发。-->
+    <!--    </aside>-->
     <div class="canvas" ref="canvas"></div>
+    <div id="js-properties-panel" class="panel"></div>
   </div>
-
 </template>
 
 <script>
   // 引入相关的依赖
   import BpmnModeler from 'bpmn-js/lib/Modeler'
-  import {
-    xmlStr
-  } from './xmlStr' // 这里是直接引用了xml字符串
+  import propertiesPanelModule from 'bpmn-js-properties-panel'
+  import propertiesProviderModule from 'bpmn-js-properties-panel/lib/provider/camunda'
+  import { xmlStr } from './xmlStr'
   export default {
-    name: 'index',
+    name: '',
     components: {},
-    // 生命周期 - 创建完成（可以访问当前this实例）
-    created() { },
-    // 生命周期 - 载入后, Vue 实例挂载到实际的 DOM 操作完成，一般在该过程进行 Ajax 交互
+// 生命周期 - 创建完成（可以访问当前this实例）
+    created() {},
+// 生命周期 - 载入后, Vue 实例挂载到实际的 DOM 操作完成，一般在该过程进行 Ajax 交互
     mounted() {
       this.init()
     },
@@ -44,44 +42,53 @@
         canvas: null
       }
     },
+// 方法集合
     methods: {
-      init() {
+      init () {
         // 获取到属性ref为“canvas”的dom节点
         const canvas = this.$refs.canvas
         // 建模
         this.bpmnModeler = new BpmnModeler({
-          container: canvas
+          container: canvas,
+          //添加控制板
+          propertiesPanel: {
+            parent: '#js-properties-panel'
+          },
+          additionalModules: [
+            // 左边工具栏以及节点
+            propertiesProviderModule,
+            // 右边的工具栏
+            propertiesPanelModule
+          ]
         })
         this.createNewDiagram()
       },
-      createNewDiagram() {
+      createNewDiagram () {
         // 将字符串转换成图显示出来
         this.bpmnModeler.importXML(xmlStr, (err) => {
           if (err) {
             // console.error(err)
-          } else {
+          }
+          else {
             // 这里是成功之后的回调, 可以在这里做一系列事情
             this.success()
           }
         })
       },
-      success() {
+      success () {
         // console.log('创建成功!')
       }
-    }
+    },
+// 计算属性
+    computed: {}
   }
 </script>
 
 <style scoped>
-  /*.components-container {*/
-  /*  position: relative;*/
-  /*  height: 100vh;*/
-  /*}*/
   .containers{
-    position: absolute;
     background-color: #ffffff;
     width: 100%;
-    height: 100%;
+    height: calc(100vh - 52px);
   }
   .canvas{
     width: 100%;
